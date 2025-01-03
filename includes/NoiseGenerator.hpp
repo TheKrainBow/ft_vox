86e1bfbcee734730abd31bc48ad69de415dfcb74
tmoragli <tmoragli@student.42.fr>
2025-01-03 21:51:23 +0100

    [NOISE] Change of structure for NoiseGenerator class

--- includes/NoiseGenerator.hpp
+++ includes/NoiseGenerator.hpp
@@ -6,21 +6,29 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 22:49:04 by tmoragli          #+#    #+#             */
-/*   Updated: 2025/01/02 22:57:21 by tmoragli         ###   ########.fr       */
+/*   Updated: 2025/01/03 21:06:55 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "ft_vox.hpp"
 
+struct NoiseData {
+	size_t seed = 42;
+	size_t nb_octaves = 4;
+	double lacunarity = 2.0;
+	double persistance = 0.5;
+};
+
 class NoiseGenerator
 {
 	public:
 		NoiseGenerator();
-		NoiseGenerator(size_t seed);
+		NoiseGenerator(NoiseData noise_data);
 		~NoiseGenerator();
-		double noise(double offset);
+		double noise(double x, double y);
 
 	private:
-		size_t _seed;
+		NoiseData data;
+		std::string _stream;
 };
--- srcs/NoiseGenerator.cpp
+++ srcs/NoiseGenerator.cpp
@@ -6,27 +6,28 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 22:51:33 by tmoragli          #+#    #+#             */
-/*   Updated: 2025/01/02 23:04:15 by tmoragli         ###   ########.fr       */
+/*   Updated: 2025/01/03 21:07:04 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "NoiseGenerator.hpp"
 
-NoiseGenerator::NoiseGenerator(size_t seed) : _seed(seed) {
-	std::cout << "The seed for this world is: " << _seed << std::endl;
+NoiseGenerator::NoiseGenerator(NoiseData noise_data) {
+	data = noise_data;
+	std::cout << "The seed for this world is: " << data.seed << std::endl;
 }
 
-NoiseGenerator::NoiseGenerator() : _seed(42) {
-	std::cout << "The seed for this world is: " << _seed << std::endl;
+NoiseGenerator::NoiseGenerator() {
+	std::cout << "The seed for this world is: " << data.seed << std::endl;
 }
-
 
 NoiseGenerator::~NoiseGenerator() {
 
 }
 
-double NoiseGenerator::noise(double offset) {
+double NoiseGenerator::noise(double x, double y) {
 	//Generate and calculate noise with given seed and offset
-	(void)offset;
+	(void)x;
+	(void)y;
 	return 0.0;
 }
