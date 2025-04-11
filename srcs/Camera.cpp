edeea893be82839b0663fbe05dc419144e5e2e4e
Mathieu Agostini <maagosti@c4r2p1.42nice.fr>
2025-04-11 13:59:32 +0200

    Reset camera to screenshot

--- srcs/Camera.cpp
+++ srcs/Camera.cpp
@@ -1,6 +1,6 @@
 #include "Camera.hpp"
 
-Camera::Camera() : position{-908, -261, -1589}, angle(0, -90) {
+Camera::Camera() : position{-820, -131, -1379}, angle(169, 12) {
 	int test = int(angle.x) / 45;
 	_facing = e_direction(test);
 };
